8541a6a62c25b17c3849d7822e07a9ae5932657e
Mark Rendle <mark@markrendle.net>
2010-01-07 22:56:14 +0000

    Initial commit

---
+++ Extensions/UIElementExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace System.Windows
+{
+    public static class UIElementExtensions
+    {
+        public static Point GetCanvasLocation(this UIElement element)
+        {
+            return new Point((double)element.GetValue(Canvas.LeftProperty), (double)element.GetValue(Canvas.TopProperty));
+        }
+
+        public static void SetCanvasLocation(this UIElement element, Point location)
+        {
+            element.SetValue(Canvas.LeftProperty, location.X);
+            element.SetValue(Canvas.TopProperty, location.Y);
+        }
+    }
+}
---
+++ SilverlightApplication1/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace SilverlightApplication1
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            Debug.WriteLine("Constructor");
+            InitializeComponent();
+            IsTabStop = true;
+            Focus();
+        }
+
+        private void UserControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            var sb = block.Resources["mover"] as Storyboard;
+            if (sb != null)
+            {
+                sb.Begin();
+            }
+        }
+
+        private void UserControl_KeyUp(object sender, KeyEventArgs e)
+        {
+            var sb = block.Resources["mover"] as Storyboard;
+            if (sb != null)
+            {
+                var pt = block.GetCanvasLocation();
+                pt = block.RenderTransform.Transform(pt);
+                sb.Stop();
+                block.SetCanvasLocation(pt);
+                block.SetValue(Canvas.LeftProperty, pt.X);
+                block.SetValue(Canvas.TopProperty, pt.Y);
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var sb = block.Resources["mover"] as Storyboard;
+            var da = sb.Children[0] as DoubleAnimation;
+            da.Completed += da_Completed;
+        }
+
+        void da_Completed(object sender, EventArgs e)
+        {
+            Debug.WriteLine("da_Completed");
+        }
+    }
+}
